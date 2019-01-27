7a4b36382c9f7fe5bcca29c68628174fa2ee01c3
missymagenta <alina.soare37@gmail.com>
2019-01-27 19:25:01 +0100

    update FLOW4 site_flow URL Configuration

--- site_flow/urls.py
+++ site_flow/urls.py
@@ -15,10 +15,11 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
-from home.views import HomepageView
+from django.urls import include, path
+
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', HomepageView.as_view()),
+    path('', include("home.urls")),
 ]
